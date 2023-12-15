7e962695ea8a01432fa855cc64fb3d81c1050e77
Sanorian <krajnov.sascha@yandex.ru>
2023-12-15 08:23:44 +0400

    Fix homework

--- src/main/scala/homework/lec3_4/Space.scala
+++ src/main/scala/homework/lec3_4/Space.scala
@@ -1,39 +1,29 @@
 package homework.lec3_4
-// Не понял задачу. Потому... вот это
+
+trait Shape
+
+case class Circle(radius: Double) extends Shape
+
+case class Square(sideLength: Double) extends Shape
+case class Rectangle(side1Length: Double, side2Length: Double) extends Shape
+case class Triangle(baseLength: Double, heightLength: Double) extends Shape
+
+case class Rhombus(diagonal1Length: Double, diagonal2Length: Double) extends Shape
+
+case class Parallerogramm(baseLength: Double, heightLength: Double) extends Shape
+case class Trapezoid(base1Length: Double, base2Length: Double, heightLength: Double) extends Shape
+
+
 object Space {
-  import scala.io.StdIn.readLine
-  def getSpace(figureType: String): Double = {
-    figureType match{
-      case "квадрат"=>{
-        val a: Int = readLine("Введите длину стороны").toInt
-        a*a
-      }
-      case "прямоугольник" => {
-        val a: Int = readLine("Введите длину первой стороны").toInt
-        val b: Int = readLine("Введите длину второй стороны").toInt
-        a * b
-      }
-      case "треугольник" => {
-        val h: Int = readLine("Введите длину высоты треугольника").toInt
-        val a: Int = readLine("Введите длину основания треугольника").toInt
-        (a * h)/2
-      }
-      case "ромб" => {
-        val d1: Int = readLine("Введите длину высоты треугольника").toInt
-        val d2: Int = readLine("Введите длину основания треугольника").toInt
-        (d2 * d1)/2
-      }
-      case "параллерограмм" => {
-        val h: Int = readLine("Введите длину высоты параллерограмма").toInt
-        val a: Int = readLine("Введите длину основания паралерограмма").toInt
-        a * h
-      }
-      case "трапеция" => {
-        val h: Int = readLine("Введите длину высоты трапеции").toInt
-        val a: Int = readLine("Введите длину первого основания трапеции").toInt
-        val b: Int = readLine("Введите длину первого основания трапеции").toInt
-        (a+b)/2 * h
-      }
+  def getSpace(shape: Shape): Double = {
+    shape match{
+      case Circle(radius)=> radius*radius*math.Pi
+      case Square(sideLength) => sideLength*sideLength
+      case Rectangle(side1Length, side2Length) => side1Length*side2Length
+      case Triangle(baseLength, heightLength) => baseLength*heightLength/2
+      case Rhombus(diagonal1Length, diagonal2Length) => diagonal1Length*diagonal2Length/2
+      case Parallerogramm(baseLength, heightLength) => baseLength*heightLength
+      case  Trapezoid(base1Length, base2Length, heightLength) => (base1Length+base2Length)/2*heightLength
     }
   }
 }
